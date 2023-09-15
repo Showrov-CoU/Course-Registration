40e061a56c1c5625cb0c3edf208786c72aca6726
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-09-16 01:51:58 +0600

    create fake json data and fetch them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Courses from "./Components/Courses/Courses";
 import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Courses></Courses>
     </>
   );
 }
---
+++ src/Components/Course/Course.jsx
@@ -0,0 +1,9 @@
+const Course = (props) => {
+  return (
+    <div>
+      <h1>Course:{props.course.courseName}</h1>
+    </div>
+  );
+};
+
+export default Course;
